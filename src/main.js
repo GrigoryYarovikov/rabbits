7fa826b1d46813291852db1d2964fa4d33e0e680
grigory <grigory.yar@ya.ru>
2017-10-03 13:25:57 +0300

    no message

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 FIELD_SIZE = 6;
 FIELD_PX = 650;
+ENABLE_RABBITS = false;
 
 CELL_SIZE = FIELD_PX/FIELD_SIZE;
 
@@ -27,23 +28,44 @@ function waitSeconds(iMilliSeconds) {
 }
 
 class Cell {
-  constructor (type) {
+  constructor (type, rabbit) {
       this.type = type;
       this.rain = 0;
       this.sun = 0;
       this.grass = 0;
+      this.rabbit = 0;
+      if (type == CellType.FIELD)
+        this.rabbit = rabbit;
+
+      this.left;
+      this.right;
+      this.up;
+      this.down;
   }
 
   update() {
+
+      if (ENABLE_RABBITS) {
+        this.eat_grass();
+      }
       this.generate_sun();
       this.generate_rain();
 
       if (this.type == CellType.FIELD) {
           this.update_grass();
+          if (ENABLE_RABBITS) {
+            this.update_rabbit();
+          }
       }
   }
 
   initialize_neighbours(left, right, up, down) {
+
+      this.left = left;
+      this.right = right;
+      this.up = up;
+      this.down = down;
+
       this.is_near_river = (left && left.type == CellType.RIVER ||
         right && right.type == CellType.RIVER ||
         up && up.type == CellType.RIVER ||
@@ -51,7 +73,7 @@ class Cell {
   }
 
   generate_sun() {
-        this.sun = Math.floor(Math.random() * 4);
+      this.sun = Math.floor(Math.random() * 4);
   }
 
   generate_rain() {
@@ -59,15 +81,29 @@ class Cell {
   }
 
   update_grass() {
-        var new_grass = this.grass;
+      var new_grass = this.grass;
 
-        if (this.is_near_river && this.sun > 0) {
-            new_grass += 1;
-        } else {
-            new_grass += WeatherMatrix[this.sun][this.rain];
-        }
+      if (this.is_near_river && this.sun > 0) {
+          new_grass += 1;
+      } else {
+          new_grass += WeatherMatrix[this.sun][this.rain];
+      }
 
-        this.grass = Math.min(Math.max(new_grass, 0), 4);
+      this.grass = Math.min(Math.max(new_grass, 0), 4);
+  }
+
+  eat_grass() {
+      //todo переселение
+      if (this.grass < this.rabbit) {
+        this.grass = 0;
+        this.rabbit = 0;
+      }
+  }
+
+  update_rabbit() {
+      this.grass -= this.rabbit;
+      if (this.rabbit == 2)
+        this.rabbit += 1;
   }
 
 };
@@ -80,12 +116,16 @@ class Row {
     generate_data(size) {
         this.cells = [];
         for (var i = 0; i < size; ++i) {
-            this.cells.push(new Cell(this.generate_cell_type()));
+            this.cells.push(new Cell(this.generate_cell_type(), this.generate_rabbits_init()));
         }
     }
 
     generate_cell_type() {
         return Math.floor(Math.random() * 3);
+    }
+
+    generate_rabbits_init() {
+        return Math.floor(Math.random() * 4);
     }
 
     update() {
@@ -140,21 +180,31 @@ class Field {
         for (var a = 0; a < this.rows.length; a++) {
             text_html += "<tr>";
             for(var b = 0; b < this.rows[a].cells.length; b++) {
-             text_html += "<td width='" + CELL_SIZE + "' height='" + CELL_SIZE + "' class='";
-             var this_cell = this.rows[a].cells[b];
-
-             text_html += get_type(this_cell.type);
-             text_html += " grass" + this_cell.grass;
-             text_html += "'>";
-             if ( this_cell.type == CellType.FIELD ) {
-               text_html += "<img src='sun.png' class='sun" + this_cell.sun + "'>";
-               text_html += "<img src='drop.png' class='rain" + this_cell.rain + "'>";
-            }
-             text_html += "</td>";
+              text_html += this.draw_cell(a, b);
           }
           text_html += "</tr>";  
         }
         field_table.innerHTML = text_html;
+    }
+
+    draw_cell(a, b) {
+      var text_html = "<td width='" + CELL_SIZE + "' height='" + CELL_SIZE + "' class='";
+      var this_cell = this.rows[a].cells[b];
+
+      text_html += get_type(this_cell.type);
+      text_html += " grass" + this_cell.grass;
+      text_html += "'>";
+      if ( this_cell.type == CellType.FIELD ) {
+        text_html += "<div><img src='sun.png' class='sun" + this_cell.sun + "'>";
+        text_html += "<img src='drop.png' class='rain" + this_cell.rain + "'></div><div>";
+        if (ENABLE_RABBITS) {
+          for (var i = 0; i < this_cell.rabbit; i++) {
+            text_html += "<img src='rabbit.png' class='rabbit'>";
+          }
+        }
+      }
+      text_html += "</div></td>";
+      return text_html;
     }
 };
 
@@ -175,6 +225,7 @@ $(document).ready(function() {
   });
 
 document.onkeypress = function(e) {
+  ENABLE_RABBITS = ENABLE_RABBITS || (e.code == 'KeyR')
   field.update();
   field.draw();
 };
