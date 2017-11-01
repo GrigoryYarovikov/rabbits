b7d642857909f2ff5ca8b4d8d17ba3d32785e3fd
grigory <grigory.yar@ya.ru>
2017-11-02 00:13:01 +0300

    vsio OK

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ FIELD_SIZE = 3;
 FIELD_PX = 650;
 ENABLE_RABBITS = false;
 
-CELL_SIZE = FIELD_PX/FIELD_SIZE;
+HUNTERS_TOTAL = 2;
+HUNTERS_INIT_MAX = 2;
+
+WOLFS_INIT_MAX = 2;
+WOLFS_TOTAL = 2;
+
+CELL_SIZE = FIELD_PX / FIELD_SIZE;
 
 CellType = {
     FIELD : 0,
@@ -17,25 +23,26 @@ var WeatherMatrix = [
     [-1, 0, 1, 1]
 ];
 
-function waitSeconds(iMilliSeconds) {
-    var counter= 0
-        , start = new Date().getTime()
-        , end = 0;
-    while (counter < iMilliSeconds) {
-        end = new Date().getTime();
-        counter = end - start;
-    }
-}
-
 class Cell {
-  constructor (type, rabbit) {
+  constructor (type, rabbits, hunters, wolfs) {
       this.type = type;
       this.rain = 0;
       this.sun = 0;
       this.grass = 0;
-      this.rabbit = 0;
-      if (type == CellType.FIELD)
-        this.rabbit = rabbit;
+      this.rabbits = 0;
+      this.hunter = 0;
+      this.wolfs = 0;
+      if (type == CellType.FIELD) {
+        this.rabbits = rabbits;
+        if (WOLFS_TOTAL > 0) {
+          WOLFS_TOTAL -= wolfs;
+          this.wolfs = wolfs;
+        }
+        if (HUNTERS_TOTAL > 0) {
+          HUNTERS_TOTAL -= hunters;
+          this.hunters = hunters;
+        }
+      }
 
       this.left;
       this.right;
@@ -50,8 +57,9 @@ class Cell {
       if (this.type == CellType.FIELD) {
           this.update_grass();
           if (ENABLE_RABBITS) {
-            this.eat_grass();
-            this.update_rabbit();
+            this.update_rabbits();
+            this.update_wolfs();
+            this.update_hunters();
           }
       }
   }
@@ -89,54 +97,159 @@ class Cell {
       this.grass = Math.min(Math.max(new_grass, 0), 4);
   }
 
-  eat_grass() {
-      if (this.grass < this.rabbit)
-      {
-          var need_rebase = this.rabbit - this.grass;
-          var base_array = [this.left, this.up, this.right, this.down];
-          this.shuffle_array(base_array);
-          need_rebase = this.take_slots(base_array[0], need_rebase);
-          need_rebase = this.take_slots(base_array[1], need_rebase);
-          need_rebase = this.take_slots(base_array[2], need_rebase);
-          need_rebase = this.take_slots(base_array[3], need_rebase);
-          this.rabbit = this.grass + need_rebase;
-      }
+  update_wolfs() {
+    if (!this.wolfs) return;
 
-      if (this.grass < this.rabbit) {
-        this.grass = 0;
-        this.rabbit = 0;
+    if (this.hunters) {
+      if (this.wolfs == this.hunters) {
+
+        var need_rebase = this.wolfs;
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_wolf_slots(base_array[0], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[1], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[2], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[3], need_rebase);
+        this.wolfs = need_rebase;
+
+        need_rebase = this.hunters;
+        base_array = this.shuffle_array();
+        need_rebase = this.take_hunter_slot(base_array[3], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[1], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[2], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[0], need_rebase);
+        this.hunters = need_rebase;
+      } else if (this.wolfs > this.hunters) {
+        this.hunters -= 1;
+
+        var need_rebase = this.hunters;
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_hunter_slot(base_array[3], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[1], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[2], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[0], need_rebase);
+        this.hunters = need_rebase;
+      } else if (this.wolfs < this.hunters) {
+        this.wolfs -= 1;
+
+        var need_rebase = this.wolfs;
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_wolf_slots(base_array[0], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[1], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[2], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[3], need_rebase);
+        this.wolfs = need_rebase;
       }
+    } else {
+      if (this.wolfs && this.rabbits) {
+        if (this.wolfs * 2 > this.rabbits) {
+          this.rabbits = 0;
+        } else {
+          this.rabbits -= this.wolfs * 2;
+        }
+
+        var need_rebase = this.rabbits;
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_rabbit_slots(base_array[0], need_rebase);
+        need_rebase = this.take_rabbit_slots(base_array[1], need_rebase);
+        need_rebase = this.take_rabbit_slots(base_array[2], need_rebase);
+        need_rebase = this.take_rabbit_slots(base_array[3], need_rebase);
+        this.rabbits = need_rebase;
+      } else {
+        var need_rebase = this.wolfs;
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_wolf_slots(base_array[0], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[1], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[2], need_rebase);
+        need_rebase = this.take_wolf_slots(base_array[3], need_rebase);
+        this.wolfs = need_rebase;
+      }
+    }
   }
 
-  shuffle_array(array) {
+  take_wolf_slots(side, need) {
+    if (side && need && side.type == CellType.FIELD) {
+        side.wolfs += 1;
+        return need - 1;
+    }
+    return need;
+  }
+
+  update_rabbits() {
+    if (!this.rabbits) return;
+    if (this.grass < this.rabbits)
+    {
+        var need_rebase = this.rabbits - this.grass;
+        
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_rabbit_slots(base_array[0], need_rebase);
+        need_rebase = this.take_rabbit_slots(base_array[1], need_rebase);
+        need_rebase = this.take_rabbit_slots(base_array[2], need_rebase);
+        need_rebase = this.take_rabbit_slots(base_array[3], need_rebase);
+        this.rabbits = this.grass + need_rebase;
+    }
+
+    if (this.grass < this.rabbits) {
+      this.grass = 0;
+      this.rabbits = 0;
+    } else {
+    this.grass -= this.rabbits;
+    if (this.rabbits == 2)
+      this.rabbits += 1;
+    }
+  }
+
+  take_rabbit_slots(side, need) {
+    if (side && need && side.rabbits < 3 && (side.grass > side.rabbits)) {
+      var free = side.grass - side.rabbits;
+      if (free > need) {
+        side.rabbits += need;
+        return 0;
+      } else {
+        side.rabbits += free;
+        return need - free;
+      }
+    }
+    return need;
+  }
+
+  update_hunters() {
+    if (!this.hunters) return;
+
+    if (this.rabbits < this.hunters)
+    {
+        var need_rebase = this.hunters - this.rabbits;
+        var base_array = this.shuffle_array();
+        need_rebase = this.take_hunter_slot(base_array[3], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[1], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[2], need_rebase);
+        need_rebase = this.take_hunter_slot(base_array[0], need_rebase);
+        this.hunters = this.rabbits + need_rebase;
+    }
+    if (this.rabbits < this.hunters) {
+      this.rabbits = 0;
+    } else {
+      this.rabbits -= this.hunters;
+    }
+  }
+
+  take_hunter_slot(side, need) {
+    if (side && need && side.type == CellType.FIELD) {
+        side.hunters += 1;
+        return need - 1;
+    }
+    return need;
+  }
+
+  shuffle_array() {
+    var array = [this.left, this.up, this.right, this.down];
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
+    return array;
   }
-
-  take_slots(side, need) {
-      if (side && side.rabbit < 3 && (side.grass > side.rabbit)) {
-        var free = side.grass - side.rabbit;
-        if (free > need) {
-          side.rabbit += need;
-          return 0;
-        } else {
-          side.rabbit += free;
-          return need - free;
-        }
-      }
-      return need;
-  }
-
-  update_rabbit() {
-      this.grass -= this.rabbit;
-      if (this.rabbit == 2)
-        this.rabbit += 1;
-  }
-
 };
 
 class Row {
@@ -147,7 +260,7 @@ class Row {
     generate_data(size) {
         this.cells = [];
         for (var i = 0; i < size; ++i) {
-            this.cells.push(new Cell(this.generate_cell_type(), this.generate_rabbits_init()));
+            this.cells.push(new Cell(this.generate_cell_type(), this.generate_rabbits_init(), this.generate_hunters_init(), this.generate_wolfs_init()));
         }
     }
 
@@ -156,7 +269,15 @@ class Row {
     }
 
     generate_rabbits_init() {
-        return Math.floor(Math.random() * 4);
+        return Math.floor(Math.random() * 3);
+    }
+
+    generate_hunters_init() {
+        return Math.floor(Math.random() * HUNTERS_INIT_MAX);
+    }
+
+    generate_wolfs_init() {
+        return Math.floor(Math.random() * WOLFS_INIT_MAX);
     }
 
     update() {
@@ -229,8 +350,14 @@ class Field {
         text_html += "<div><img src='sun.png' class='sun" + this_cell.sun + "'>";
         text_html += "<img src='drop.png' class='rain" + this_cell.rain + "'></div><div>";
         if (ENABLE_RABBITS) {
-          for (var i = 0; i < this_cell.rabbit; i++) {
+          for (var i = 0; i < this_cell.rabbits; i++) {
             text_html += "<img src='rabbit.png' class='rabbit'>";
+          }
+          for (var i = 0; i < this_cell.wolfs; i++) {
+            text_html += "<img src='wolf.png' class='wolf'>";
+          }
+          for (var i = 0; i < this_cell.hunters; i++) {
+            text_html += "<img src='hunter.png' class='hunter'>";
           }
         }
       }
